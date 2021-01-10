44d5184719bec537a4d25dcaa0e5ac105252f5f1
Zirouan <rudsonlive@gmail.com>
2021-01-10 20:07:13 -0300

    validator login

--- app/src/main/java/com/zirouan/unphoto/screen/login/validator/LoginValidatorHelper.kt
+++ app/src/main/java/com/zirouan/unphoto/screen/login/validator/LoginValidatorHelper.kt
@@ -7,11 +7,11 @@ import com.zirouan.unphoto.base.BaseValidatorHelper
 class LoginValidatorHelper(private val context: Context) : BaseValidatorHelper {
 
     override fun validate(vararg any: Any?): String? {
-        val username = any[0] as? String
+        val email = any[0] as? String
         val password = any[1] as? String
 
         return when {
-            username.isNullOrEmpty() ->
+            email.isNullOrEmpty() ->
                 context.getString(R.string.type_your_email)
             password.isNullOrEmpty() ->
                 context.getString(R.string.type_your_password)
